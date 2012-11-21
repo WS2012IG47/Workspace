9064c7146149e82fc20f3c910ffbe59d06f85fb1
Padombar <pdthiele@gmail.com>
2012-11-21 21:57:03 +0100

    Test mit einer neuen Klasse

---
+++ ADS/uebung03/NeueKlasse.java
@@ -0,0 +1,13 @@
+package uebung03;
+
+public class NeueKlasse {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Boah, voll die Klasse ey!");
+	}
+
+}
