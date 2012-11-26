24ebbb0535778fde35da516e258090a57b1647c9
Padombar <pdthiele@gmail.com>
2012-11-26 19:22:01 +0100

    Fertig machen!!!!

--- GDI/uebung07/TShirtVersand.java
+++ GDI/uebung07/TShirtVersand.java
@@ -3,5 +3,9 @@ package uebung07;
 interface TShirtVersand{
 	String rechnungen(Personendaten kunde, int bestellnummer);	 
 	
-	String bestellung(Personendaten kunde, int bestellnummer, int artikelnummer )
+	String bestellung(Personendaten kunde, int bestellnummer, int artikelnummer, int anzahl, int bezahlung)
+	
+	String bestellstatus(int artikelnummer, int anzahl, )
+	
+	
 }
