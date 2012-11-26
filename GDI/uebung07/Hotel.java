54b09d2fef5c9029dbc6a85ea40cafb435c58763
p.dombrowski <pdthiele@gmail.com>
2012-11-26 15:18:23 +0100

    hallo

--- GDI/uebung07/Hotel.java
+++ GDI/uebung07/Hotel.java
@@ -2,35 +2,38 @@ package uebung07;
 
 interface Hotel{
 /**
- * Ein Zimmer für einen gewissen Zeitraum
- * @param anreise
- * @param abreise
- * @param artikelnummer
- * @param kunde
- * @param kreditkarte
- * @param extrawünsche
- * @return
+ * Ein Zimmer für einen gewissen Zeitraum reservieren
+ * @param anreise Das Datum der Anreise um zu wissen, ab wann das Zimmer reserviert werden soll.
+ * @param abreise Das Datum der Abreise um zu wissen, bis wann das Zimmer reserviert werden soll.
+ * @param artikelnummer Um den Umfang der Reserrvierung zu wissen. Wie z.B. Vollpension, Halbpension,...
+ * @param kunde Die Daten des Kunden um zu wissen, für wen das Zimmer reserviert ist.
+ * @param kreditkarte Die Kreditkartendaten werden zur Authentifizierung des Kunden bei der Bestätigung benötigt.
+ * @param extrawünsche Hier drin werden eventuelle Extrawünsche des Kunden gespeichert.
+ * @return Als Bestätigung erhält der Kunde eine Reservierungsnummer.
  */
 	int reservierung (String anreise, String abreise, int artikelnummer, Personendaten kunde, KKDaten kreditkarte, String extrawünsche);
+
 /**
- * 	
- * @param reservierungsnummer
- * @param kunde
- * @return
+ * Falls der Kunde seine Reservierung stornieren möchte. 	
+ * @param reservierungsnummer Hieraus werden die Daten wie der Zeitraum der Reservierung,... benötigt.
+ * @param kunde Um zu wissen, wer die Zimmerreservierung storniert.
+ * @return Der Kunde erhält eine Stornierungsbestätigung.
  */
 	String stornieren(int reservierungsnummer, Personendaten kunde);
+
 /**
- * 	
- * @param kreditkarte
- * @param reservierungsnummer
- * @return
+ * 	Wenn der Kunde per Kreditkarte zahlen möchte, werden diese Daten benötigt.
+ * @param kreditkarte Hierin sind die Kreditkartendaten des SKunden gespeichert.
+ * @param reservierungsnummer Um den Preis berechnen zu können, werden die Daten des Aufenthaltes benötigt
+ * @return Zahlungsquittung
  */
 	String bezahlen(KKDaten kreditkarte, int reservierungsnummer);
+
 /**
- * 	
- * @param ecKarte
- * @param reservierungsnummer
- * @return
+ * 	Wenn der Kunde per EC-Karte zahlen möchte, werden diese Daten benötigt.
+ * @param ecKarte Hierin sind die EC-Karte des SKunden gespeichert.
+ * @param reservierungsnummer Um den Preis berechnen zu können, werden die Daten des Aufenthaltes benötigt
+ * @return Zahlungsquittung
  */
 	String bezahlen(ECKarte ecKarte, int reservierungsnummer);
 /**
@@ -70,5 +73,21 @@ interface Hotel{
  */
 	void bestellungAufsZimmer(int artikelnummer, int reservierungsnummer);
 	
-	
+}
+kunde{
+}
+
+kreditkarte{
+	String name;
+	String vorname;
+	String kontonummer;
+	int sicherheitscode;
+}
+eckarte{
+	String name;
+	String vorname;
+	String kontonummer;
+	int bankleitzahl;
+	String bic;
+	String iban;
 }
