36eb552df9cdabeedf0f4f3e2ed9728c1486c627
Leon <fuxfell.leon@googlemail.com>
2012-11-26 20:14:38 +0100

    überarbeitete ausgabe

--- ADS/uebung03/ShakeSort.java
+++ ADS/uebung03/ShakeSort.java
@@ -72,7 +72,7 @@ public class ShakeSort {
 					unsorted = true;
 
 					// print numbers if LOOP finished ONCE.
-					println(makeMarkedArray(targetArray, i, i + 1));
+					println(makeMarkedArray(targetArray, i - 1, i));
 			}
 		}
 	}
@@ -82,9 +82,13 @@ public class ShakeSort {
 		String out = "";
 		int i = 0;
 		do {
-			if (i == k || i == j) {
-				out += "|" + a[i] + "|";
-			} else {
+			if (i == k) {
+				out += "{" + a[i] + "-";
+			}
+			else if(i == j){
+				out += "-" + a[i] + "}";
+			}
+			else {
 				out += " " +  a[i] + " ";
 			}
 		} while (++i < a.length);
